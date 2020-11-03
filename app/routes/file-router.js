ddfeef05f48ad8d08aefa6ff9ca133d8178bce96
AdreyViv <andrey.vivbox@gmail.com>
2020-11-03 10:15:58 +0200

    Add authentication middleware before uploading files

--- app/routes/file-router.js
+++ app/routes/file-router.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 const files = require("../controllers/file-controller.js");
+const auth = require('../middleware/authentication.js');
 
 
 router.get('/', files.allFiles);
-router.post('/upload', files.uploadFile);
+router.post('/upload', auth, files.uploadFile);
 router.get('/:id', files.findFile);
 router.delete('/:id', files.deleteFile);
 router.get('/download/:id', files.downloadFile);
